790604e8b1be4c595f3c02a16da71e0b2197a1bd
ryan-neil <ryanjosephneil@gmail.com>
2022-04-29 13:44:58 -1000

    wip: added react query to search view

--- src/views/Search/Search.js
+++ src/views/Search/Search.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-// helpers
-import useDebounce from '../../hooks/useDebounce';
+import { useQuery } from 'react-query';
+// components
+import FetchLoading from '../../components/FetchLoading';
+import FetchError from '../../components/FetchError';
 // styles
 import {
   StyledSearch,
@@ -13,59 +15,59 @@ import {
 import { Container, StyledButton, StyledInput, SearchBarIcon } from '../../styles/Utils.styled';
 
 const Search = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const debouncedSearchTerm = useDebounce(searchTerm, 1000);
+  const [inputValue, setInputValue] = useState('');
+  const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   const navigate = useNavigate();
 
+  // fetch spots API from react query
+  const { isLoading, error, data } = useQuery('spotsData', () =>
+    fetch('/api/spots').then((res) => res.json())
+  );
+
+  // mounted data checks
+  if (isLoading) return <FetchLoading />;
+  if (error) return <FetchError />;
+
   /**
-   * Handle user search:
-   * fetch api
-   * filter for matching text (clean the users text input)
-   * return filtered results back to client
+   * Handle user search
+   *
    */
-  const handleSearch = async (search) => {
-    try {
-      const res = await fetch('/api/spots');
-      const data = await res.json();
-
-      const filteredResults = data.filter((item) =>
-        item.name.toLowerCase().includes(search.toLowerCase().trim())
-      );
-
-      return filteredResults;
-    } catch (err) {
-      throw new Error('Search Error: ', err);
-    }
+  const handleSearch = (e) => {
+    // set the input value to users input
+    setInputValue(e.target.value);
+    // get users' searched word
+    setSearchText(e.target.value);
+    // filter for matching spots
+    const filteredResults = data.filter((item) =>
+      item.name.toLowerCase().includes(searchText.toLowerCase().trim())
+    );
+    // update search state
+    searchText === '' ? setSearchResults([]) : setSearchResults(filteredResults);
   };
 
   /**
-   * The effect for the api:
-   * check for user searching and call handle search logic and set the search results
-   * if no results close the dropdown
-   * dependency array: only call effect if debounced search term changes
+   * Handle user selection
+   *
    */
-  useEffect(() => {
-    if (debouncedSearchTerm) {
-      handleSearch(debouncedSearchTerm).then((data) => {
-        setResults(data);
-      });
-    } else {
-      setResults([]);
-    }
-  }, [debouncedSearchTerm]);
+  const handleClick = (result) => {
+    // set input value to clicked result
+    setInputValue(`${result.name}, ${result.location.state}`);
+    // close the dropdown
+    setSearchResults([]);
+  };
 
   /**
    * Render the search results:
    * set input result logic and styles
    * handle redirect to Forecast page on user selection
    */
-  const handleResults = results.length !== 0 && (
+  const handleResults = searchResults.length !== 0 && (
     <StyledInputResults>
-      {results.map((result) => (
+      {searchResults.map((result) => (
         <li key={result.spot_id}>
-          <Link to={`/forecast/${result.slug}`} onClick={() => setResults([])}>
+          <Link to={`/forecast/${result.slug}`} onClick={() => handleClick(result)}>
             <p>{result.name}</p>
             <p className="results-location">{`${result.location.county}, ${result.location.state}`}</p>
           </Link>
@@ -84,7 +86,8 @@ const Search = () => {
             <input
               type="text"
               placeholder="Search spot..."
-              onChange={(e) => setSearchTerm(e.target.value)}
+              value={inputValue}
+              onChange={handleSearch}
             />
           </StyledInput>
           <StyledButton type="submit" onClick={() => navigate(-1)}>
